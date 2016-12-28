28556448191cea1d89d90e727a3221552c6f90d8
Reptation <david@waldenpeakfarm.com>
2016-12-28 18:56:31 -0500

    spacing on the headings. v1.0 complete

--- testmaker/models.py
+++ testmaker/models.py
@@ -4,16 +4,12 @@ from django.db import models
 
 class Question(models.Model):
     text = models.CharField(max_length=256)
-#    choices = models.ManyToManyField(Choice)
     def __str__(self):
         return self.text
-#    def display_choices(self):
-#        pass 
 
 
 class Choice(models.Model):
     text = models.CharField(max_length=128)
-    #question = models.ForeignKey(Question, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     is_correct = models.BooleanField()    
     def __str__(self):
@@ -27,8 +23,5 @@ class Test(models.Model):
     helpful_relations = models.CharField(max_length=512)
     questions = models.ManyToManyField(Question)
     choices = models.ManyToManyField(Choice)
-    #don't think choices should be necessary here
     def __str__(self):
         return self.class_name
-
-
--- testmaker/views.py
+++ testmaker/views.py
@@ -1,33 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from reportlab.pdfgen import canvas
 from django.http import HttpResponse
-from io import BytesIO
 from testmaker.models import Question, Test, Choice
-
-#for testing
-import datetime
-
-# Create your views here.
-
-def print_pdf(request):
-    buffer = BytesIO()
-    response = HttpResponse(content_type='application/pdf')
-    p = canvas.Canvas(buffer)
-    now = datetime.datetime.now()    
-    p.drawString(100, 100, "hello testmaker, it is %s." % now)
-    p.showPage()
-    p.save()
-
-    pdf = buffer.getvalue()
-    buffer.close()
-    response.write(pdf)
-    return response
 
 def q_and_c(request):
     question = get_object_or_404(Question, pk=1)
-    #choices = question.choice_set.all()
-    #html = "<html><body>The question is %s.</body></html>" % question
-    #return HttpResponse(html)
     return render(request, 'testmaker/q_and_c.html', {'question': question})
 
 def t_and_q(request):
@@ -36,9 +12,6 @@ def t_and_q(request):
     choices = Choice.objects.filter(question__in=questions)
     return render(request, 'testmaker/t_and_q.html', {'test': test, 'questions': questions, 'choices': choices})   
 
-
-
-#http://stackoverflow.com/questions/1377446/render-html-to-pdf-in-django-site
 import cStringIO as StringIO
 from xhtml2pdf import pisa
 from django.template.loader import get_template
@@ -58,24 +31,11 @@ def render_to_pdf(template_src, context_dict):
     return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
 
 
-#def template_test(request, test_id='2'):
 def template_test(request, test_id='2'):
-#    print ("function template test\n")
-#    print(test_id)
     my_id = test_id[0]
     print my_id
     test = get_object_or_404(Test, id=my_id)
     questions = Question.objects.filter(test__id=my_id)
     choices = Choice.objects.filter(question__in=questions)
     return render_to_pdf('testmaker/t_and_q.html', {'test': test, 'questions': questions, 'choices': choices, 'pagesize': 'A4'})   
-
-#    #Retrieve data or whatever you need
-#    return render_to_pdf(
-#            'testmaker/template_test.html',
-#            {
-#                'pagesize':'A4',
-##                'mylist': results,
-#            }
-#        )
-
 
